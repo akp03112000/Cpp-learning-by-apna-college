0b014af449120ee2ea1f77cd83beb397ab2f17a6
akhilesh12114739 <akp03112000@gmail.com>
2024-10-18 19:05:01 +0530

    even odd and largest of two number

---
+++ basics/09largestoftwonumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b;
+  cout<<"enter 1st number: ";
+  cin>>a;
+  cout<<"enter 2nd number: ";
+  cin>>b;
+  if(a>b){
+    cout<<"Largest number is: "<<a;
+  }
+  else{
+    cout<<"largest number is: "<<b;
+  }
+  
+  return 0;
+}
---
+++ basics/10evenorodd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cout << "enter 1st number: ";
+  cin >> a;
+
+  if (a % 2 == 0)
+  {
+    cout << "its a even number";
+  }
+  else
+  {
+    cout << "its a odd number";
+  }
+  return 0;
+}
