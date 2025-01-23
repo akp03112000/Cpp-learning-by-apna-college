f4756e63445588b62bd5b744d94cfdba5f88947a
akhilesh12114739 <akp03112000@gmail.com>
2025-01-23 14:44:51 +0530

    input and output in 2D array

--- Array/2D array/02inoutoutput.cpp
+++ Array/2D array/02inoutoutput.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
-  int students[2][2] = {{1, 2}, {3, 4}};
+  int students[2][2];
   int n = 2;
   int m = 2;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout << "enter the elements: ";
+      cin >> students[i][j];
+    }
+  }
+
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < m; j++)
